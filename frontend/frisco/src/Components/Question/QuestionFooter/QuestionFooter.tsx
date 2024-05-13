4029e6dee84226e282818d50ca3c9a920d59710d
Skwst <31214849+Skwst@users.noreply.github.com>
2024-05-13 12:16:29 +0300

    switch button order

--- frontend/frisco/src/Components/Question/QuestionFooter/QuestionFooter.tsx
+++ frontend/frisco/src/Components/Question/QuestionFooter/QuestionFooter.tsx
@@ -11,15 +11,14 @@ const QuestionFooter: React.FC<QuestionFooterProps> = ({ onClickNext, onClickBac
 
     return (
         <div className="flex  w-full justify-between gap-8">
-
-            <button onClick={onClickNext} className="px-4 py-2 bg-frisco_purple text-white font-semibold rounded transition-colors shadow-md hover:bg-frisco_purple_light focus:outline-none">
-                <p>Next</p>
-            </button>
             {!isFirstQuestion && (
                 <button onClick={onClickBack} className="px-4 py-2 bg-neutral-500 text-white font-semibold rounded transition-colors shadow-md hover:bg-neutral-600 focus:outline-none">
                     Back
                 </button>
             )}
+            <button onClick={onClickNext} className="px-4 py-2 bg-frisco_purple text-white font-semibold rounded transition-colors shadow-md hover:bg-frisco_purple_light focus:outline-none">
+                <p>Next</p>
+            </button>
         </div>
     )
 }
