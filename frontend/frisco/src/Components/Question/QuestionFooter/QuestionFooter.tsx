94580b094141120b1bef91ffc193096f82dd098b
Skwst <31214849+Skwst@users.noreply.github.com>
2024-05-13 12:23:30 +0300

    add gap when there is no back button

--- frontend/frisco/src/Components/Question/QuestionFooter/QuestionFooter.tsx
+++ frontend/frisco/src/Components/Question/QuestionFooter/QuestionFooter.tsx
@@ -10,11 +10,13 @@ interface QuestionFooterProps {
 const QuestionFooter: React.FC<QuestionFooterProps> = ({ onClickNext, onClickBack, isFirstQuestion }) => {
 
     return (
-        <div className="flex  w-full justify-between gap-8">
-            {!isFirstQuestion && (
+        <div className="flex  w-full justify-between items-center gap-8">
+            {!isFirstQuestion ? (
                 <button onClick={onClickBack} className="px-4 py-2 bg-neutral-500 text-white font-semibold rounded transition-colors shadow-md hover:bg-neutral-600 focus:outline-none">
                     Back
                 </button>
+            ) : (
+                <div className="flex-grow"/>
             )}
             <button onClick={onClickNext} className="px-4 py-2 bg-frisco_purple text-white font-semibold rounded transition-colors shadow-md hover:bg-frisco_purple_light focus:outline-none">
                 <p>Next</p>
